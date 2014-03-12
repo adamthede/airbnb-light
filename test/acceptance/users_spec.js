34a8f13876561b116382e74c0f47c9bd100f7bb9
Adam Thede <adam@thedetech.com>
2014-03-12 14:06:27 -0500

    registration for user unit and acceptance tests complete

---
+++ test/acceptance/users_spec.js
@@ -0,0 +1,71 @@
+'use strict';
+
+process.env.DBNAME = 'airbnb-test';
+var request = require('supertest');
+//var fs = require('fs');
+//var exec = require('child_process').exec;
+var app = require('../../app/app');
+var expect = require('chai').expect;
+var User;
+//var cookie;
+var u1;
+
+describe('user', function(){
+  before(function(done){
+    request(app)
+    .get('/')
+    .end(function(err, res){
+      User = require('../../app/models/user');
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    global.nss.db.dropDatabase(function(err, result){
+      u1 = new User({email:'athede@nomail.com', password:'1234', role:'host'});
+      u1.register(function(){
+        done();
+      });
+    });
+  });
+
+  describe('GET /register', function(){
+    it('should display the register page', function(done){
+      request(app)
+      .get('/register')
+      .end(function(err, res){
+        expect(res.status).to.equal(200);
+        expect(res.text).to.include('Register');
+        done();
+      });
+    });
+  });
+
+  describe('POST /register', function(){
+    it('should register a new user', function(done){
+      request(app)
+      .post('/register')
+      .field('email', 'adam_thede@yahoo.com')
+      .field('password', '1234')
+      .field('role', 'host')
+      .end(function(err, res){
+        expect(res.status).to.equal(302);
+        expect(res.text).to.include('Moved Temporarily. Redirecting to /');
+        done();
+      });
+    });
+
+    it('shold not register an already existing email address', function(done){
+      request(app)
+      .post('/register')
+      .field('email', 'athede@nomail.com')
+      .field('password', '1234')
+      .field('role', 'host')
+      .end(function(err, res){
+        expect(res.status).to.equal(200);
+        expect(res.text).to.include('Register');
+        done();
+      });
+    });
+  });
+});
