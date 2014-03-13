30cb6b3c517254e2f602116b1f41267a6d167a6f
Adam Thede <adam@thedetech.com>
2014-03-13 17:42:43 -0500

    introducing and including geo with the google maps api

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -15,12 +15,17 @@ module.exports = function(req, res, next){
 function load(app, fn){
   var home = require('../routes/home');
   var users = require('../routes/users');
+  var listings = require('../routes/listings');
 
   app.get('/', d, home.index);
   app.get('/register', d, users.register);
   app.post('/register', d, users.create);
   app.get('/login', d, users.login);
   app.post('/login', d, users.auth);
+  app.get('/listings/new', d, listings.fresh);
+  app.post('/listings', d, listings.create);
+  app.get('/listings', d, listings.index);
+  app.get('/listings/query', d, listings.query);
   console.log('Routes Loaded');
   fn();
 }
